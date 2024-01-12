079102601ee3163605ea940082c7aae6f373d9a0
Yess-P <parkes9509@gmail.com>
2024-01-12 17:16:09 +0900

    fix: 주석처리
    사용하지 않는 코드

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -8,7 +8,7 @@ import (
 
 type Store interface {
 	Querier
-	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+	//TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
 }
 
 // SQLStore provides all functions to execute SQL queries and transactions
@@ -46,46 +46,46 @@ func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) erro
 
 // TransferTx performs a money transfer from one account to the other
 // It creates a transfer record, add account entries, and update accounts' balance within a single database transaction
-func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
-	var result TransferTxResult
-
-	err := store.execTx(ctx, func(q *Queries) error {
-		var err error
-		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
-			FromAccountID: arg.FromAccountID,
-			ToAccountID:   arg.ToAccountID,
-			Amount:        arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
-			AccountID: arg.FromAccountID,
-			Amount:    -arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
-			AccountID: arg.ToAccountID,
-			Amount:    arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		// get account -> update its balance
-		if arg.FromAccountID < arg.ToAccountID {
-			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
-		} else {
-			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
-		}
-		return nil
-	})
-
-	return result, err
-}
+//func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+//	var result TransferTxResult
+//
+//	err := store.execTx(ctx, func(q *Queries) error {
+//		var err error
+//		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+//			FromAccountID: arg.FromAccountID,
+//			ToAccountID:   arg.ToAccountID,
+//			Amount:        arg.Amount,
+//		})
+//		if err != nil {
+//			return err
+//		}
+//
+//		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+//			AccountID: arg.FromAccountID,
+//			Amount:    -arg.Amount,
+//		})
+//		if err != nil {
+//			return err
+//		}
+//		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+//			AccountID: arg.ToAccountID,
+//			Amount:    arg.Amount,
+//		})
+//		if err != nil {
+//			return err
+//		}
+//
+//		// get account -> update its balance
+//		if arg.FromAccountID < arg.ToAccountID {
+//			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+//		} else {
+//			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+//		}
+//		return nil
+//	})
+//
+//	return result, err
+//}
 
 func addMoney(
 	ctx context.Context,
